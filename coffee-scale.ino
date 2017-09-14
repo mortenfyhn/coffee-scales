e1b4519dbb84a1b5413496b435064acfb2da07de
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2017-09-14 20:04:07 +0200

    Tare on boot

--- coffee-scale.ino
+++ coffee-scale.ino
@@ -14,6 +14,7 @@
 
 #define SCALE_FACTOR 1874
 #define SCALE_OFFSET 984550
+#define TARE_AVERAGES 10
 
 TimerDisplay    timerDisplay(DISP_TIMER_CLK, DISP_TIMER_DIO);
 GramsDisplay    gramsDisplay(DISP_SCALE_CLK, DISP_SCALE_DIO);
@@ -28,7 +29,7 @@ void setup()
   // Load cell
   scale.begin(SCALE_DT, SCALE_SCK);
   scale.set_scale(SCALE_FACTOR);
-  scale.set_offset(SCALE_OFFSET);
+  scale.tare(TARE_AVERAGES);
 }
 
 
