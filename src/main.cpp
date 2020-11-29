f073ddf8a2c0570d6a0576d1aaf4d2082458c97b
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2020-11-29 17:45:23 +0100

    Implement tare button
    Fix 41

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,12 @@
 
 // Pin connections
 constexpr uint8_t hx711_dt = 2;            // D2
-constexpr uint8_t hx711_sck = 3;           // D3
+constexpr uint8_t hx711_sck = 4;           // D3
 constexpr uint8_t timer_display_clk = 12;  // D12
 constexpr uint8_t timer_display_dio = 11;  // D11
 constexpr uint8_t scale_display_clk = 15;  // A1
 constexpr uint8_t scale_display_dio = 16;  // A2
+constexpr uint8_t tare_button = 3;         // INT0
 
 // Configuration
 constexpr float hx711_scale_factor = 1874.f;
@@ -24,6 +25,12 @@ auto timer_display =
     TimerDisplay{timer_display_clk, timer_display_dio, brightness};
 auto weight_display = Display{scale_display_clk, scale_display_dio, brightness};
 auto filter = SmoothingFilter{filter_size, hysteresis_size};
+volatile bool should_reset = false;
+
+void button_pushed()
+{
+    should_reset = true;
+}
 
 void setup()
 {
@@ -31,10 +38,20 @@ void setup()
     scales.set_scale(hx711_scale_factor);
     scales.tare(hx711_tare_samples / 2);
     scales.tare(hx711_tare_samples / 2);
+
+    pinMode(tare_button, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(tare_button), button_pushed, FALLING);
 }
 
 void loop()
 {
+    if (should_reset)
+    {
+        scales.tare(1);
+        timer_display.stop();
+        should_reset = false;
+    }
+
     filter.addValue(scales.get_units());
     const auto weight_in_grams = filter.getValue();
 
