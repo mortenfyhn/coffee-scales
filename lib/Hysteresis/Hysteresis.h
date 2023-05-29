a58b1e6611dc17c5f72539da71e5c9d1339c42b9
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2023-05-29 19:46:34 +0200

    Hysteresis: Make it actually work
    This makes the hysteresis work as intended...
    
    Makes the displayed weight a lot more stable

--- lib/Hysteresis/Hysteresis.h
+++ lib/Hysteresis/Hysteresis.h
@@ -3,35 +3,17 @@
 class Hysteresis
 {
   public:
-    Hysteresis(float threshold_size) : threshold_size(threshold_size) {}
+    Hysteresis(float threshold) : threshold(threshold) {}
 
-    float compute(float value)
+    float compute(float input)
     {
-        if (value > upper_threshold)
-            increaseThreshold(value);
-        else if (value < lower_threshold)
-            decreaseThreshold(value);
+        if (input > output + threshold || input < output - threshold)
+            output = input;
 
-        return curr_value;
+        return output;
     }
 
   private:
-    const float threshold_size = 0.f;
-    float upper_threshold = 0.f;
-    float lower_threshold = 0.f;
-    float curr_value = 0.f;
-
-    void increaseThreshold(float new_upper_threshold)
-    {
-        upper_threshold = new_upper_threshold;
-        lower_threshold = upper_threshold - threshold_size;
-        curr_value = new_upper_threshold;
-    }
-
-    void decreaseThreshold(float new_lower_threshold)
-    {
-        lower_threshold = new_lower_threshold;
-        upper_threshold = lower_threshold + threshold_size;
-        curr_value = new_lower_threshold;
-    }
+    float output = 0.f;
+    const float threshold;
 };
