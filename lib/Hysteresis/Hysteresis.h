b6723b733c6c34cbea9a583775d258344bfca30d
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2023-05-29 19:36:38 +0200

    Hysteresis: Rewrite to header-only

--- lib/Hysteresis/Hysteresis.h
+++ lib/Hysteresis/Hysteresis.h
@@ -3,14 +3,35 @@
 class Hysteresis
 {
   public:
-    Hysteresis(float threshold_size);
-    float compute(float value);
+    Hysteresis(float threshold_size) : threshold_size(threshold_size) {}
+
+    float compute(float value)
+    {
+        if (value > upper_threshold)
+            increaseThreshold(value);
+        else if (value < lower_threshold)
+            decreaseThreshold(value);
+
+        return curr_value;
+    }
 
   private:
     const float threshold_size = 0.f;
     float upper_threshold = 0.f;
     float lower_threshold = 0.f;
     float curr_value = 0.f;
-    void increaseThreshold(float new_upper_threshold);
-    void decreaseThreshold(float new_lower_threshold);
+
+    void increaseThreshold(float new_upper_threshold)
+    {
+        upper_threshold = new_upper_threshold;
+        lower_threshold = upper_threshold - threshold_size;
+        curr_value = new_upper_threshold;
+    }
+
+    void decreaseThreshold(float new_lower_threshold)
+    {
+        lower_threshold = new_lower_threshold;
+        upper_threshold = lower_threshold + threshold_size;
+        curr_value = new_lower_threshold;
+    }
 };
