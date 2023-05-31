7a976ef9a716e7e65753160a8f1266205c218e09
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2023-05-31 07:28:15 +0200

    Update processing script

--- logs/process.m
+++ logs/process.m
@@ -1,5 +1,7 @@
 ## Load data
-input_file = "raw/brew.csv";
+input_filename = "brew.csv";
+% input_filename = "steadystate.csv";
+input_file = fullfile("raw", input_filename);
 input_data = csvread(input_file);
 time = input_data(2:end, 1);
 raw_data = input_data(2:end, 2);
@@ -11,7 +13,19 @@ after_scaling = (raw_data - offset) / scale;
 
 ## Running average filter
 filter_size = 10;
-after_smoothing = filter(ones(filter_size, 1) / filter_size, 1, after_scaling);
+standard_deviation_threshold = 0.1;
+in = after_scaling;
+out = zeros(size(in));
+filtered = filter(ones(filter_size, 1) / filter_size, 1, in);
+for i = 1:numel(in)
+    stddev = std(filtered(max(1, i - filter_size):i));
+    if stddev < standard_deviation_threshold
+        out(i) = filtered(i);
+    else
+        out(i) = in(i);
+    end
+end
+after_smoothing = out;
 
 ## Hysteresis
 function output = hysteresis(input, threshold, previous_output)
@@ -26,17 +40,18 @@ end
 
 hysteresis_size = 0.1;
 previous_output = 0;
-input_data = after_smoothing;
-output_data = zeros(size(input_data));
-for i = 1:numel(input_data)
-    output_data(i) = hysteresis(input_data(i), hysteresis_size, previous_output);
-    previous_output = output_data(i);
+in = after_smoothing;
+out = zeros(size(in));
+for i = 1:numel(in)
+    out(i) = hysteresis(in(i), hysteresis_size, previous_output);
+    previous_output = out(i);
 end
-after_hysteresis = output_data;
+after_hysteresis = out;
 
 ## Write result to file
 mkdir("processed");
-file = fopen("processed/brew.csv", "w");
+output_file = fullfile("processed", input_filename);
+file = fopen(output_file, "w");
 fprintf(file, "time,raw_data,1_after_scaling,2_after_smoothing,3_after_hysteresis\n");
 for i = 1:numel(time)
     fprintf(
@@ -49,3 +64,4 @@ for i = 1:numel(time)
         after_hysteresis(i));
 end
 fclose(file);
+
